6472fcf2838ee517775390666ac2045aae22233c
alexandre michiels <alexoualexandre1@gmail.com>
2025-10-07 11:26:04 +0200

    link

--- src/Teste.jsx
+++ src/Teste.jsx
@@ -1,5 +1,5 @@
 function Teste(){
-return (<h1> mieux que sa ! </h1>)
+return (<h1> <a href="https://www.youtube.com/watch?v=0EVVKs6DQLo&list=RDMI9ZpIKgyf0&index=5"> mieux que sa ! </a></h1>)
 }
 
 export default Teste;

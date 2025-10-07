088eb96479b24740d890f927fe45007734646231
alexandre michiels <alexoualexandre1@gmail.com>
2025-10-07 11:22:44 +0200

    ref

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Outlet, Link } from "react-router-dom";
 export default function App() {
   return (
     <div>
-      <h1>Bienvenue enfoiré</h1>
+      <h1>réfléchis</h1>
 
       <nav>
         <ul>

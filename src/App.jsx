2f5f2c9af30956c3c0ca7b06897f08f3ef176bef
alexandre michiels <alexoualexandre1@gmail.com>
2025-10-11 19:39:05 +0200

    head

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
   return (
     <>
       <header>
-        <h1 style={{ color: "white" }}>header</h1>
+        <h1 style={{ color: "white" }}>head</h1>
 
         <nav>
           <ul className="ul-nav">

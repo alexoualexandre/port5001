10fd28dcc8dbe50960ef960849edb3a023330873
alexandre michiels <alexoualexandre1@gmail.com>
2025-10-11 11:10:48 +0200

    mise en place express

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import { Outlet, Link } from "react-router-dom";
 
 export default function App() {
+  const reponseServer = "réponse du serveur : port 3000 en écoute !";
+  const request = () => {
+    fetch(`http://localhost:3000/teste/${reponseServer}`)
+      .then((response) => response.json())
+      .then((resp) => {
+        console.log(resp.reponse);
+      });
+  };
   return (
     <div>
       <h1>fixe</h1>
@@ -16,6 +24,9 @@ export default function App() {
           </li>
         </ul>
       </nav>
+      <button type="button" onClick={request}>
+        request
+      </button>
       <Outlet />
     </div>
   );
